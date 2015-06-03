c4e534eb8eaaa6eabc184ed125a6fb1e40d4c7bc
nise81 <niels.seidel@nise81.com>
2015-06-03 13:40:16 +0200

    0
    Former-commit-id: 935e386c16ea9fd0ed3530518430a65d25b82ec9

---
+++ commit.sh
@@ -0,0 +1,8 @@
+git add .
+if zenity --entry \
+--title="Commit Message" \
+--text="Enter a commit message" \
+--entry-text "Neues Profil"
+then git commit -m "$?"; git push origin master
+  else echo "You did not provide a commit message. Commit and push failed."
+fi
